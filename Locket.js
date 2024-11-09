74482c4c903edb68c4720702e71c33a58bfa2a79
23hnHieu <89006969+23hnHieu@users.noreply.github.com>
2024-11-09 16:03:34 +0900

    Update Locket.js

--- Locket.js
+++ Locket.js
@@ -9,33 +9,33 @@ var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
   obj = JSON.parse($response.body);
 obj.Attention =
   "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
-var ohoang7 = {
+var bmhieu = {
     is_sandbox: !1,
     ownership_type: "PURCHASED",
     billing_issues_detected_at: null,
     period_type: "normal",
-    expires_date: "2099-12-18T01:04:17Z",
+    expires_date: "3000-12-04T01:04:17Z",
     grace_period_expires_date: null,
     unsubscribe_detected_at: null,
-    original_purchase_date: "2024-07-28T01:04:18Z",
-    purchase_date: "2024-07-28T01:04:17Z",
+    original_purchase_date: "2030-12-04T01:04:18Z",
+    purchase_date: "2030-12-04T01:04:17Z",
     store: "app_store",
   },
-  vuong2023 = {
+  hieu2006 = {
     grace_period_expires_date: null,
-    purchase_date: "2024-07-28T01:04:17Z",
-    product_identifier: "com.ohoang7.premium.yearly",
-    expires_date: "2099-12-18T01:04:17Z",
+    purchase_date: "2030-12-04T01:04:17Z",
+    product_identifier: "com.bmhieu4126.premium.yearly",
+    expires_date: "3000-12-04T01:04:17Z",
   };
 const match = Object.keys(mapping).find((e) => ua.includes(e));
 if (match) {
   let [e, s] = mapping[match];
   s
-    ? ((vuong2023.product_identifier = s),
-      (obj.subscriber.subscriptions[s] = ohoang7))
-    : (obj.subscriber.subscriptions["com.ohoang7.premium.yearly"] = ohoang7),
-    (obj.subscriber.entitlements[e] = vuong2023);
+    ? ((hieu2006.product_identifier = s),
+      (obj.subscriber.subscriptions[s] = bmhieu))
+    : (obj.subscriber.subscriptions["com.bmhieu4126.premium.yearly"] = bmhieu),
+    (obj.subscriber.entitlements[e] = hieu2006);
 } else
-  (obj.subscriber.subscriptions["com.ohoang7.premium.yearly"] = ohoang7),
-    (obj.subscriber.entitlements.pro = vuong2023);
+  (obj.subscriber.subscriptions["com.bmhieu4126.premium.yearly"] = bmhieu),
+    (obj.subscriber.entitlements.pro = heiu2006);
 $done({ body: JSON.stringify(obj) });
